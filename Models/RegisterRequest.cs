e0463147d835c04805aed3940ffd58830df1cae9
Dariel delgado <darieldelga2@gmail.com>
2022-05-03 18:53:43 -0500

    RegisterRequest model

--- Models/RegisterRequest.cs
+++ Models/RegisterRequest.cs
@@ -7,5 +7,10 @@ namespace TaskManager.Models
 {
     public class RegisterRequest
     {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
     }
 }
